d189ec89f2f3385c54194d6a10e0baf94e6ce7d3
Muhammad Qasim <developermqasim@outlook.com>
2025-06-26 15:37:42 +0500

    ...

--- code/task_1_uvc/tb/router_test_lib.sv
+++ code/task_1_uvc/tb/router_test_lib.sv
@@ -9,12 +9,18 @@ class base_test extends uvm_test;
     router_tb tb;
 
     function void build_phase(uvm_phase phase);
+
+        //  uvm_config_wrapper::set(this, "<path>.run_phase",
+        //     "default_sequence",
+        //     yapp_5_packets::get_type());
+
         tb = new("tb", this);
         `uvm_info(get_type_name(), "Build phase of test is being executed", UVM_HIGH);
     endfunction: build_phase
 
     function void end_of_elaboration_phase(uvm_phase phase);
         uvm_top.print_topology();
+        uvm_sequencer.print_topology();
     endfunction: end_of_elaboration_phase
 
 endclass: base_test
